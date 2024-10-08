4510a72df8d4be9e686d2a2f7bd01f257a82e94b
hbahramian <habahram@iu.edu>
2024-10-08 14:34:11 -0400

    .

---
+++ src/main/java/edu/iu/p466/prime_service/controller/PrimesController.java
@@ -0,0 +1,11 @@
+package edu.iu.p466.prime_service.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class PrimesController {
+
+}
