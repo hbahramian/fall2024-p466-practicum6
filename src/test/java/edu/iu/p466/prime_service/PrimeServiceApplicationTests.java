d617ef970cc40eeea478e1672c1202f533d789b0
hbahramian <habahram@iu.edu>
2024-11-25 14:07:05 -0500

    .

--- src/test/java/edu/iu/p466/prime_service/PrimeServiceApplicationTests.java
+++ src/test/java/edu/iu/p466/prime_service/PrimeServiceApplicationTests.java
@@ -8,21 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PrimeServiceApplicationTests {
 
-	@Value("${spring.datasource.url}")
-	private String datasourceUrl;
 
-	@Value("${spring.datasource.username}")
-	private String datasourceUsername;
-
-	@Value("${spring.datasource.password}")
-	private String datasourcePassword;
-
-	@Test
-    void logDBConnection() {
-		System.out.println("Database URL: " + datasourceUrl);
-		System.out.println("Database username: " + datasourceUsername);
-		System.out.println("Database password: " + datasourcePassword);
-	}
 
 	@Test
 	void contextLoads() {
--- src/test/java/edu/iu/p466/prime_service/service/PrimeServiceTest.java
+++ src/test/java/edu/iu/p466/prime_service/service/PrimeServiceTest.java
@@ -1,10 +1,27 @@
 package edu.iu.p466.prime_service.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrimeServiceTest {
+
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+
+    @Value("${spring.datasource.username}")
+    private String datasourceUsername;
+
+    @Value("${spring.datasource.password}")
+    private String datasourcePassword;
+
+    @Test
+    void logDBConnection() {
+        System.out.println("Database URL: " + datasourceUrl);
+        System.out.println("Database username: " + datasourceUsername);
+        System.out.println("Database password: " + datasourcePassword);
+    }
 
     @Test
     void _45IsNotPrime() {
