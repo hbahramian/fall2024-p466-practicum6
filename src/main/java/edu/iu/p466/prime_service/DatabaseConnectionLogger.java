a875d5dafef74dd5ff59bf76535061b1947c96d1
hbahramian <habahram@iu.edu>
2024-11-24 19:10:20 -0500

    .

---
+++ src/main/java/edu/iu/p466/prime_service/DatabaseConnectionLogger.java
@@ -0,0 +1,19 @@
+package edu.iu.p466.prime_service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class DatabaseConnectionLogger {
+
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+
+    @PostConstruct
+    public void logDatabaseConnection() {
+        System.out.println("Database URL: " + datasourceUrl);
+    }
+}
+
