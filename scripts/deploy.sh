aac40c91483f00535af6fa99182bd886da9708d5
hbahramian <habahram@iu.edu>
2024-11-25 16:48:42 -0500

    .

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,3 @@
-#
-# Deploys the microservice to Kubernetes.
-#
-# Assumes the image has already been built and published to the container registry.
-#
-# Environment variables:
-#
-#   CONTAINER_REGISTRY - The hostname of your container registry.
-#   VERSION - The version number of the image to deploy.
-#
-# Usage:
-#
-#   ./deploy.sh
-#
 
 set -u # or set -o nounset
 : "$CONTAINER_REGISTRY"
@@ -19,10 +5,5 @@ set -u # or set -o nounset
 : "$SPRING_DATASOURCE_URL"
 : "$SPRING_DATASOURCE_UN"
 : "$SPRING_DATASOURCE_PW"
-
-
-
-#envsubst < ./scripts/kubernetes/deploy.yaml | kubectl delete -f -
-
 
 envsubst < ./scripts/kubernetes/deploy.yaml | kubectl apply -f -
