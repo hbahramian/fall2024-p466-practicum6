7ce267df2f3574c55f04968f4fba07656fac8233
hbahramian <habahram@iu.edu>
2024-11-25 22:41:21 -0500

    .

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,8 @@
 set -u # or set -o nounset
 : "$CONTAINER_REGISTRY"
 : "$VERSION"
-: "$SPRING_DATASOURCE_URL"
-: "$SPRING_DATASOURCE_UN"
-: "$SPRING_DATASOURCE_PW"
+: "$DATASOURCE_URL"
+: "$DATASOURCE_UN"
+: "$DATASOURCE_PW"
 
 envsubst < ./scripts/kubernetes/deploy.yaml | kubectl apply -f -
